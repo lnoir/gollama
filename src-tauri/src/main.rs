cbdbb0d52ed232dffd6e34a2153b0438834a157e
lnoir <gh@latn.black>
2023-10-26 10:17:46 +0100

    fix: unable to make requests in production build
    A couple of different issues caused this:
    - Ollama only allows requests from `http://localhost` and `0.0.0.0` by default,
      but Tauri apps use a host with a custom protocol: tauri://localhost. Fixed
      by adding the @tauri-apps/plugins-workspace localhost plugin.
    - Content Security Policy was not set correctly.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{utils::config::AppUrl, WindowUrl};
 
 fn main() {
+  let port = portpicker::pick_unused_port().expect("failed to find unused port");
+
+  let mut context = tauri::generate_context!();
+  let url = format!("http://localhost:{}", port).parse().unwrap();
+  let window_url = WindowUrl::External(url);
+
+  // rewrite the config so the IPC is enabled on this URL
+  context.config_mut().build.dist_dir = AppUrl::Url(window_url.clone());
+
   tauri::Builder::default()
-    .run(tauri::generate_context!())
+    .plugin(tauri_plugin_localhost::Builder::new(port).build())
+    .run(context)
     .expect("error while running tauri application");
 }
